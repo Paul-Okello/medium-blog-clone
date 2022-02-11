1ca1862f19f58c1440d5fa95cc4d5d62067003bf
Paul-Okello <paulokello273@gmail.com>
2022-02-11 12:07:49 +0300

    💥 NEW: main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,73 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
 
-export default function Home() {
+interface Props {
+  posts: [Post]
+}
+export default function Home({ posts }: Props) {
   return (
-    <div className="">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <Hero />
+      {/* Posts */}
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                src={urlFor(post.mainImage).url()!}
+                alt="Post Main Image"
+                className="h-60 w-full   object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+              />
+              <div className="flex justify-between bg-white p-5">
+                <div className="">
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-sm ">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = `
+    *[_type == "post"]{
+  _id,
+  title,
+  slug,
+  author -> {
+  name,
+  image
+},
+description,
+mainImage
+}`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
