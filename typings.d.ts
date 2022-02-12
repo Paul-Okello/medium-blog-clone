39bee2656121e5e799e669bf0a70a5d0236da201
Paul-Okello <paulokello273@gmail.com>
2022-02-12 11:59:52 +0300

    ⚡ General Update:  Comment Typing

--- typings.d.ts
+++ typings.d.ts
@@ -21,4 +21,21 @@ export interface Post {
     current: string
   }
   title: string
+  comments: Comment[]
+}
+
+export interface Comment {
+  approved: boolean
+  comment: string
+  email: string
+  name: string
+  post: {
+    _ref: string
+    _type: string
+  }
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
 }
